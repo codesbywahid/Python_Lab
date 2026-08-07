a3b6f4a4fda84dfdf2d14a1880f637388d8a2c77
Abdul Wahid <aw3559113@gmail.com>
2026-08-07 15:06:57 +0500

    Guess the number Game Mini Project Added

---
+++ GuessNumber.py
@@ -0,0 +1,26 @@
+
+import random
+secret_number = random.randint(1, 100)
+guess = 0
+attempts = 0
+
+while guess != secret_number:
+    try:
+        guess = int(input("Enter your guess number (1-100): "))
+        attempts += 1
+
+        if guess < 1 or guess > 100:
+            print("Please enter a number between 1 and 100.")
+            continue
+
+        if guess > secret_number:
+            print("Too high! Choose a smaller number.")
+        elif guess < secret_number:
+            print("Too low! Choose a bigger number.")
+        else:
+            print("🎉 Congratulations! You guessed it.")
+            print(f"You guessed the number in {attempts} attempts.")
+
+    except ValueError:
+        print("Invalid input! Please enter a whole number.")
+        print(f"You guessed the number in {attempts} attempts.")
