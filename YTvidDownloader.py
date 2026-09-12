d547f47e44a039dae31fdd36f8a7198b68d8b349
Abdul Wahid <aw3559113@gmail.com>
2026-09-12 15:45:14 +0500

    YouTube video downloader

---
+++ YTvidDownloader.py
@@ -0,0 +1,16 @@
+from pytube import YouTube
+from sys import argv
+
+link = argv[1]
+yt = YouTube(link)
+
+print("Title : ", yt.title)
+print("Views : ", yt.views)
+
+yd = yt.streams.get_highest_resolution()
+
+# Change the path below to the folder where you want the video saved
+# Example (Windows): 'C:/Users/YourName/Downloads'
+# Example (Mac/Linux): '/Users/YourName/Downloads'
+# Or use '.' to save in the same folder as this script
+yd.download('.')
