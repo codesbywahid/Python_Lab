fd3eccdf642cc5381f59daf2847758a4c4d17081
Abdul Wahid <aw3559113@gmail.com>
2026-09-15 20:51:43 +0500

    RestApi created with db

---
+++ RestApi.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///drinks.db"
+
+db = SQLAlchemy(app)
+
+
+class Drink(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(
+        db.String(80),
+        unique=True,
+        nullable=False
+    )
+
+    description = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f"{self.name}-{self.description}"
+
+
+@app.route("/")
+def index():
+    return "Hello"
+
+
+@app.route("/drinks")
+def get_drinks():
+    return {"drinks": "drinks data"}
